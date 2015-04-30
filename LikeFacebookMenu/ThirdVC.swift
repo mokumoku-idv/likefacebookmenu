a5d5cf5ce1301f423f42e0639afbc03fb8e4124d
hiratsukashu <hiratsukashu@indival.co.jp>
2015-04-30 14:29:42 +0900

    完成！

---
+++ LikeFacebookMenu/ThirdVC.swift
@@ -0,0 +1,43 @@
+//
+//  ThirdVC.swift
+//  LikeFacebookMenu
+//
+//  Created by 平塚 俊輔 on 2015/04/30.
+//  Copyright (c) 2015年 平塚 俊輔. All rights reserved.
+//
+
+import UIKit
+
+class ThirdVC: UIViewController {
+
+    @IBOutlet weak var leftbutton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        if self.revealViewController() != nil {
+            leftbutton.target = self.revealViewController()
+            leftbutton.action = Selector("revealToggle:")
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
